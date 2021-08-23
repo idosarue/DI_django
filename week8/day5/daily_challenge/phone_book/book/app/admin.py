edbe802cd20ed1ce1a64cd70aa5af5073ebc054e
ido sarue <dodojoker67@gmail.com>
2021-08-23 13:59:49 +0300

    done

--- week8/day5/daily_challenge/phone_book/book/app/admin.py
+++ week8/day5/daily_challenge/phone_book/book/app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Person
+from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
+from phonenumber_field.modelfields import PhoneNumberField
+from django.db import models
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone_number', 'address')
     search_fields = ['name','email', 'phone_number']
-   
+    formfield_overrides = {
+        models.CharField: {'widget': PhoneNumberInternationalFallbackWidget},
+    }
 admin.site.register(Person, PersonAdmin)
